e9dc5730f9500b86fc669269bbc9a762d794f174
DaniilSh23 <danyasevas11@gmail.com>
2022-06-21 18:44:13 +0300

    check_the_FSM

--- handlers/head_page.py
+++ handlers/head_page.py
@@ -22,9 +22,10 @@ async def start_command_handler(message: types.Message):
     await message.answer(text=text_for_message, reply_markup=MAIN_MENU)
 
 
-async def head_page(call: CallbackQuery, callback_data: dict):
+async def head_page(call: CallbackQuery, callback_data: dict, state: FSMContext):
     '''Реакция бота на инлайн кнопку ГЛАВНАЯ.'''
 
+    await state.reset_state()
     await call.answer(text=f'{emojize(":robot:")}Вы перешли на главную')
     text_for_message = f'{emojize(":robot:")}<b>Вы находитесь на главной</b>\n\n'\
                        f'{emojize(":service_dog:")}<b>Всегда готов</b> оказать помощь в любой ситуации.\n'
@@ -77,6 +78,6 @@ async def send_users_message(message: types.Message, state: FSMContext):
 def register_head_page_handlers():
     DP.register_message_handler(start_command_handler, Command(['start', 'help']))
     DP.register_message_handler(my_problem, Text(equals=KEYBOARD['MY_PROBLEM']))
+    DP.register_callback_query_handler(head_page, call_head_page.filter(flag='head_page'), state=SupportMessageState.users_message)
     DP.register_callback_query_handler(head_page, call_head_page.filter(flag='head_page'))
-    DP.register_callback_query_handler(head_page, call_base_answer.filter(flag='head_page'))
     DP.register_message_handler(send_users_message, state=SupportMessageState.users_message)
